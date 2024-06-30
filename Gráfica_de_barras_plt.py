301ef14a0474b452aa20bcbd39d946188975d46f
milagrospozzofasini <milagrospozzofasini@gmail.com>
2024-06-30 19:59:20 -0300

    Commit

--- Gráfica_de_barras_plt.py
+++ Gráfica_de_barras_plt.py
@@ -1,9 +1,6 @@
-# Importar el módulo pyplot con el alias plt
 import matplotlib.pyplot as plt
-from collections import Counter
 
-# Suponemos que ya tenemos definida la lista 'estudiantes'
-# Inicializar una lista para almacenar los datos de los estudiantes
+# Datos predeterminados de los estudiantes
 estudiantes = [
     {"nombre": "Arriola, María", "calificaciones": [10, 8, 6, 10, 9]},
     {"nombre": "Buffa, Lucas", "calificaciones": [10, 9, 8, 6, 9]},
@@ -11,29 +8,46 @@ estudiantes = [
     {"nombre": "Noy, Antonhy", "calificaciones": [6, 6, 7, 8, 9]}
 ]
 
-def grafico_barras_notas(estudiantes):
-    # Recopilar todas las notas en una lista
-    todas_las_notas = [nota for estudiante in estudiantes for nota in estudiante['calificaciones']]
-    
-    # Contar la frecuencia de cada nota
-    contador = Counter(todas_las_notas)
-    
-    # Preparar los datos para el gráfico
-    notas = list(contador.keys())
-    frecuencias = list(contador.values())
-    
-    # Crear el gráfico de barras
+def control_promedio_notas_estudiantes(estudiantes):
+    # Inicializamos listas vacías para almacenar los nombres y promedios
+    nombres = []
+    promedios = []
+
+    # Iteramos sobre cada estudiante en la lista
+    for estudiante in estudiantes:
+        # Agregamos el nombre del estudiante a la lista de nombres
+        nombres.append(estudiante['nombre'])
+        
+        # Calculamos el promedio de calificaciones del estudiante
+        promedio = sum(estudiante['calificaciones']) / len(estudiante['calificaciones'])
+        
+        # Agregamos el promedio calculado a la lista de promedios
+        promedios.append(promedio)
+
+    # Definimos los colores
+    colores_hex = ['#FFA07A', '#98FB98', '#87CEFA', '#DDA0DD', '#F0E68C']
+
+    # Creamos una nueva figura con un tamaño específico
     plt.figure(figsize=(10, 6))
-    plt.bar(notas, frecuencias)
-    
-    # Personalizar el gráfico
-    plt.title('Distribución de Notas')
-    plt.xlabel('Notas')
-    plt.ylabel('Frecuencia')
-    plt.xticks(range(min(notas), max(notas)+1))
-    
-    # Mostrar el gráfico
+
+    # Creamos el gráfico de barras con colores personalizados
+    plt.bar(nombres, promedios, color=colores_hex[:len(estudiantes)])
+
+    # Personalizamos el gráfico
+    plt.title('Control de Promedio de Notas de los Estudiantes')
+    plt.xlabel('Estudiantes')
+    plt.ylabel('Promedio de Calificaciones')
+    plt.ylim(0, 10)
+
+    # Añadimos los valores de los promedios sobre cada barra
+    for i, v in enumerate(promedios):
+        plt.text(i, v, f'{v:.2f}', ha='center', va='bottom')
+
+    # Ajustamos automáticamente el diseño para que todo quede bien
+    plt.tight_layout()
+
+    # Mostramos el gráfico
     plt.show()
 
-# Llamar a la función para crear el gráfico
-grafico_barras_notas(estudiantes)
+# Llamamos a la función con nuestros datos de estudiantes
+control_promedio_notas_estudiantes(estudiantes)
